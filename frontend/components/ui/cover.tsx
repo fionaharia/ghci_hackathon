6399765277f7d037b62dfdab1e226d8411725d0a
fionaharia <hariafiona@gmail.com>
2024-11-03 16:44:12 +0530

    service completed

--- frontend/components/ui/cover.tsx
+++ frontend/components/ui/cover.tsx
@@ -223,7 +223,7 @@ export const CircleIcon = ({
   return (
     <div
       className={cn(
-        `pointer-events-none animate-pulse group-hover/cover:hidden group-hover/cover:opacity-100 group h-2 w-2 rounded-full bg-neutral-600 dark:bg-white opacity-20 group-hover/cover:bg-white`,
+        `pointer-events-none animate-pulse group-hover/cover:hidden group-hover/cover:opacity-100 group h-2 w-2 rounded-full bg-black dark:bg-white opacity-20 group-hover/cover:bg-white`,
         className
       )}
     ></div>
